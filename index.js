5bfe936517c7f159f88121b1371f6d19944843d0
Sreejith KS <sreejithksgupta2255@gmail.com>
2023-04-27 18:29:14 +0530

    before the big merger, sreejith 13

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 homeslider();
 addbtns();
+document.getElementById('homepage').style.display = "flex";
 
 
 
@@ -17,15 +18,18 @@ function homeslider(){
 
 
 function addbtns(){
-    document.getElementById("navbtnhome").addEventListener("click", function(){console.log("home")});
-    document.getElementById("navbtnknow").addEventListener("click", function(){console.log("knowledge base")});
-    document.getElementById("navbtnprof").addEventListener("click", function(){console.log("Profile")});
-    document.getElementById("NGOlistCTAbtn").addEventListener("click", function(){console.log("NGO list")});
-    document.getElementById("homeCFP").addEventListener("click", function(){console.log("Calculate Carbon Credit")});
+    document.getElementById("navbtnhome").addEventListener("click", function(){switchtopage("homepage")});
+    document.getElementById("navbtnknow").addEventListener("click", function(){switchtopage("knowledge")});
+    document.getElementById("navbtnprof").addEventListener("click", function(){switchtopage("Profile")});
+    document.getElementById("NGOlistCTAbtn").addEventListener("click", function(){switchtopage("ngopage")});
+    document.getElementById("homeCFP").addEventListener("click", function(){switchtopage("CFPcalculcpage")});
 }
 
 
 function switchtopage(page){
-      document.getElementById("content").innerHTML = "";
-        document.getElementById("content").innerHTML = page;
+   var pages = document.getElementsByClassName('pageItem');
+ for(var i = 0; i < pages.length; i++){
+        pages[i].style.display = "none";
+    }
+      document.getElementById(page).style.display = "flex";
 }
